a774c64ad1d4866a02bce70c1bc86d50a7283337
marinIsabel <95246946+marinIsabel@users.noreply.github.com>
2022-01-31 23:26:33 +0100

    PRODUCTO
    PRODUCTO

--- src/components/ProductoDescripcion.vue
+++ src/components/ProductoDescripcion.vue
@@ -1,13 +1,8 @@
 <template>
-  <section id="producto-descripcion">
-      <h1>DESCRIPCIÓN DEL PRODUCTO</h1>
-      <ul>
-          <li>HUAWEI MateBook X es pequeño pero poderoso; su procesador de hasta 7ª generación Intel® Core™ i7 proporciona potencia suficiente tanto para trabajo como para entretenimiento. </li>
-          <li>La ingeniería aeroespacial permite al HUAWEI MateBook X permanecer frío gracias a sus materiales de cambio de fase (usados en la industria espacial), lo que significa que el sobrecalentamiento es una cosa del pasado.</li>
-          <li>El botón de encendido dos en uno del HUAWEI MateBook X y su sensor de reconocimiento por huella dactilar te ayudan a arrancarlo de forma rápida y segura para funcionar a la velocidad de tu vida.</li>
-          <li>Seguridad total. Hoy en día utilizamos los dispositivos electrónicos para todo tipo de cosas; desde transacciones bancarias hasta el almacenamiento de documentos importantes. Por ello, la seguridad nunca está de más. El portátil Huawei está equipado con un sensor de huella dactilar para que sólo tú puedas iniciar sesión y utilizarlo. </li>
-          <li>Memoria ultrarrápida. Con el disco duro de 256 GB SSD podrás arrancar tu Windows 10 como un verdadero tiro. En un abrir y cerrar de ojos tendrás listo tu portátil.  </li>
-      </ul>
+  <section class="center" id="producto-descripcion">
+    <div> <fa class="fa" icon="sync-alt"></fa> Garantía de sustitución 24h</div>
+    <div><fa icon="door-closed"></fa> Envío gratuito en pedidos superiores a 50€</div>
+    <div><fa icon="history"></fa> Recibe tu pedido en 24/48h</div>
   </section>
 </template>
 
@@ -18,11 +13,17 @@ export default {
 </script>
 
 <style>
-#producto-descripcion {
-    margin:1rem;
+
+section#producto-descripcion{
+      background: rgb(255, 215, 142);
+      margin: 0;
+      padding: 1.3rem;
 }
-#producto-descripcion h1 {
-    color: #1abc9c;
+.center{  
+    display:flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
 }
 
 </style>
