2f1f71db8b20dcab90b0238e76e0966e6c9dcb6b
marinIsabel <95246946+marinIsabel@users.noreply.github.com>
2022-01-31 23:23:39 +0100

    migas
    migas

--- src/components/CaminoMigas.vue
+++ src/components/CaminoMigas.vue
@@ -1,11 +1,12 @@
 <template>
   <section id="camino-migas">
-      <a class="inicio" href="#"> Inicio </a> >
-      <a href="#">Ordenadores </a> > 
-      <a class="actual" href="#">Ordenador portátil</a> 
-
+      <a class="inicio" href="#">  <fa icon="bars"></fa>  Todas las categorías </a> >
+      <a class="inicio" href="#"> Home </a> >
+      <a href="#">Smartphones y Telefonía </a> > 
+      <a class="actual" href="#">Ver todos los Accesorios</a> 
 
   </section>
+  <hr>
 </template>
 
 <script>
@@ -18,22 +19,28 @@ export default {
 section#camino-migas {
     margin-left: 1rem;
     display:flex;
+    flex-wrap: wrap;
+    align-items: center;
+ 
 
 }
 section#camino-migas a, 
 section#camino-migas a:hover, 
 section#camino-migas a:visited{
     text-decoration: none;
-    font-size: 1.2rem;
+    font-size: 0.7rem;
     margin-left: 1rem;
     margin-right: 1rem;
-    color: #1abc9c;
+    color: #363636;
 }
 section#camino-migas a.actual {
     font-weight: 700;
 }
 section#camino-migas {
-    margin-left: 1rem;
-    color: #1abc9c;
+    margin-left: 0.7rem;
+    color: #363636;
+}
+hr {
+  border-color: rgb(46, 46, 46);
 }
 </style>
